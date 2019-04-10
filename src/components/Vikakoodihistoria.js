4cbdd3c1ff96302818a49337afbc22ced76e54dd
rikuojanen <35731097+rikuojanen@users.noreply.github.com>
2019-04-10 08:43:24 +0300

    onon
    onh

--- src/components/Vikakoodihistoria.js
+++ src/components/Vikakoodihistoria.js
@@ -1,99 +1,93 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Paper from '@material-ui/core/Paper';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Icon from '@material-ui/core/Icon';
-import {NavLink} from "react-router-dom";
-import Grid from '@material-ui/core/Grid';
-import {IconButton} from "@material-ui/core";
-import ValitseHuoltamo from "./valitsehuoltamo";
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Paper from "@material-ui/core/Paper";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Icon from "@material-ui/core/Icon";
+import { NavLink } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import { IconButton } from "@material-ui/core";
 
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const styles = theme => ({
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3
+    }
+  },
 
-    layout: {
-        width: 'auto',
-        marginLeft: theme.spacing.unit * 2,
-        marginRight: theme.spacing.unit * 2,
-        [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
-            width: 600,
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
-    },
-    paper: {
-        marginTop: theme.spacing.unit * 3,
-        marginBottom: theme.spacing.unit * 3,
-        padding: theme.spacing.unit * 2,
-        [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
-            marginTop: theme.spacing.unit * 6,
-            marginBottom: theme.spacing.unit * 6,
-            padding: theme.spacing.unit * 3,
-        },
-    },
-
-
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-    },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular
+  }
 });
 
-
-
 class Vikakoodit extends React.Component {
+  render() {
+    const { classes } = this.props;
 
+    return (
+      <React.Fragment>
+        <CssBaseline />
 
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Typography component="h5" variant="h5" align="center">
+              Vikakoodit
+            </Typography>
 
-    render() {
-        const { classes } = this.props;
-
-        return (
-            <React.Fragment>
-                <CssBaseline />
-
-
-                <main className={classes.layout}>
-                    <Paper className={classes.paper}>
-                        <Typography component="h5" variant="h5" align="center">
-                             Vikakoodit
-                        </Typography>
-
-                        <ExpansionPanel>
-                            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                                <Typography className={classes.heading}>Vikakoodi:12312</Typography>
-                            </ExpansionPanelSummary>
-                            <ExpansionPanelDetails>
-                                <Typography>
-                                    Vikakoodi: 12312<br/>
-                                    Aika:  12.3.2019<br/>
-                                    Selite: Öljykorkin lika     <br/>
-                                    Vakavuus: Tosi vakava!
-                                </Typography>
-                            </ExpansionPanelDetails>
-                        </ExpansionPanel>
-
-                    </Paper>
-                </main>
-            </React.Fragment>
-        );
-    }
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography className={classes.heading}>
+                  Vikakoodi:12312
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>
+                  Vikakoodi: 12312
+                  <br />
+                  Aika: 12.3.2019
+                  <br />
+                  Selite: Öljykorkin lika <br />
+                  Vakavuus: Tosi vakava!
+                </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Paper>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 Vikakoodit.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Vikakoodit);
