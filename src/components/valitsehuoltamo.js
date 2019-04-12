d9dfdf423450631f0280b93ff8e7e15dd8cfd530
rikuojanen <35731097+rikuojanen@users.noreply.github.com>
2019-04-12 15:32:09 +0300

    on
    on

--- src/components/valitsehuoltamo.js
+++ src/components/valitsehuoltamo.js
@@ -1,60 +1,60 @@
 /* eslint-disable react/prop-types, react/jsx-handler-names */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 
 const styles = {
-    root: {
-        width: 300,
-    },
-    slider: {
-        padding: '22px 0px',
-    },
+  root: {
+    width: 300
+  },
+  slider: {
+    padding: "22px 0px"
+  }
 };
 
-
-
 class ValitseHuoltamo extends React.Component {
-    state = {
-        value: 10,
-        maxvalue: 100,
-        minvalue:1
-    };
+  state = {
+    value: 10,
+    maxvalue: 100,
+    minvalue: 1
+  };
 
-    handleChange = (event, value) => {
-        this.setState({ value });
-        console.log(value)
-    };
+  handleChange = (event, value) => {
+    this.setState({ value });
+    console.log(value);
+  };
 
-    render() {
-        const { classes } = this.props;
-        const { value } = this.state;
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
     return (
-        <div>
-            <TextField
-                required
-                id="Huoltamo"
-                name="Huoltamo"
-                label="Huoltamo"
-                fullWidth
-
-            /> Tähän react-select tai react-autosuggest
-
-
-
-        </div>
+      <div>
+        Valitse Huoltamo
+        <TextField
+          required
+          id="Paikkakunta"
+          name="Paikkakunta"
+          label="Paikkakunta"
+          fullWidth
+        />
+        Tähän react-select tai react-autosuggest
+        <TextField
+          required
+          id="Huoltamo"
+          name="Huoltamo"
+          label="Huoltamo"
+          fullWidth
+        />
+      </div>
     );
+  }
 }
-}
-
-
 
 ValitseHuoltamo.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(ValitseHuoltamo);
--- src/components/varaahuolto.js
+++ src/components/varaahuolto.js
@@ -1,183 +1,205 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Paper from '@material-ui/core/Paper';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Icon from '@material-ui/core/Icon';
-import {NavLink} from "react-router-dom";
-import Grid from '@material-ui/core/Grid';
-import {IconButton} from "@material-ui/core";
+import React from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Paper from "@material-ui/core/Paper";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Icon from "@material-ui/core/Icon";
+import { NavLink } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import { IconButton } from "@material-ui/core";
 import ValitseHuoltamo from "./valitsehuoltamo";
 
-
 const styles = theme => ({
-    appBar: {
-        position: 'relative',
-    },
-    barText:{
-        position:'absolute',
-
-
-    },
-    layout: {
-        width: 'auto',
-        marginLeft: theme.spacing.unit * 2,
-        marginRight: theme.spacing.unit * 2,
-        [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
-            width: 600,
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
-    },
-    paper: {
-        marginTop: theme.spacing.unit * 3,
-        marginBottom: theme.spacing.unit * 3,
-        padding: theme.spacing.unit * 2,
-        [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
-            marginTop: theme.spacing.unit * 6,
-            marginBottom: theme.spacing.unit * 6,
-            padding: theme.spacing.unit * 3,
-        },
-    },
-    stepper: {
-        padding: `${theme.spacing.unit * 3}px 0 ${theme.spacing.unit * 5}px`,
-    },
-    buttons: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-    },
-    button: {
-        marginTop: theme.spacing.unit * 3,
-        marginLeft: theme.spacing.unit,
-    },
+  appBar: {
+    position: "relative"
+  },
+  barText: {
+    position: "absolute"
+  },
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3
+    }
+  },
+  stepper: {
+    padding: `${theme.spacing.unit * 3}px 0 ${theme.spacing.unit * 5}px`
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+  button: {
+    marginTop: theme.spacing.unit * 3,
+    marginLeft: theme.spacing.unit
+  }
 });
 
-const steps = ['Valitse huoltamo', 'Ajanvaraus', 'Varmistus'];
+const steps = [" ", "", ""];
 
 function getStepContent(step) {
-    switch (step) {
-        case 0:
-            return <div style={{margin:"auto"}}><ValitseHuoltamo/></div>;
-        case 1:
-            return "joku kalenteri ratkaisu";
-        case 2:
-            return "Varauksen tiedot";
-        default:
-            throw new Error('Error');
-    }
+  switch (step) {
+    case 0:
+      return (
+        <div style={{ margin: "auto" }}>
+          <ValitseHuoltamo />
+        </div>
+      );
+    case 1:
+      return "joku kalenteri ratkaisu";
+    case 2:
+      return "Varauksen tiedot";
+    default:
+      throw new Error("Error");
+  }
 }
-var bgColors = { "Default": "#81b71a",
-    "Blue": "#3F51B5",
-    "Cyan": "#37BC9B",
-    "Green": "#8CC152",
-    "Red": "#E9573F",
-    "white": "#fff",
+var bgColors = {
+  Default: "#81b71a",
+  Blue: "#3F51B5",
+  Cyan: "#37BC9B",
+  Green: "#8CC152",
+  Red: "#E9573F",
+  white: "#fff"
 };
 class Checkout extends React.Component {
-    state = {
-        activeStep: 0,
-    };
+  state = {
+    activeStep: 0
+  };
 
-    handleNext = () => {
-        this.setState(state => ({
-            activeStep: state.activeStep + 1,
-        }));
-    };
+  handleNext = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep + 1
+    }));
+  };
 
-    handleBack = () => {
-        this.setState(state => ({
-            activeStep: state.activeStep - 1,
-        }));
-    };
+  handleBack = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1
+    }));
+  };
 
-    handleReset = () => {
-        this.setState({
-            activeStep: 0,
-        });
-    };
+  handleReset = () => {
+    this.setState({
+      activeStep: 0
+    });
+  };
 
+  render() {
+    const { classes } = this.props;
+    const { activeStep } = this.state;
 
-    render() {
-        const { classes } = this.props;
-        const { activeStep } = this.state;
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Toolbar
+          style={{
+            backgroundColor: bgColors.Blue,
+            color: bgColors.white,
+            padding: "4.5%"
+          }}
+        >
+          <IconButton component={NavLink} to={"/"}>
+            <Icon style={{ color: bgColors.white }}>arrow_back</Icon>
+          </IconButton>
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Typography
+              variant="h4"
+              color="inherit"
+              className={classes.barText}
+            >
+              Varaa huolto
+            </Typography>
+          </Grid>
+        </Toolbar>
 
-        return (
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Typography component="h5" variant="h5" align="center">
+              Huollon varaus
+            </Typography>
+            <Stepper activeStep={activeStep} className={classes.stepper}>
+              <Step key="1">
+                <StepLabel />
+              </Step>
+              <Step key="2">
+                <StepLabel />
+              </Step>
+              <Step key="3">
+                <StepLabel />
+              </Step>
+            </Stepper>
             <React.Fragment>
-                <CssBaseline />
-                <Toolbar style={{backgroundColor: bgColors.Blue,color: bgColors.white,padding:"4.5%"}}>
-                    <IconButton component={NavLink} to={"/"}><Icon style={{color:bgColors.white}}>arrow_back</Icon></IconButton>
-                    <Grid
-                        container
-                        direction="column"
-                        justify="center"
-                        alignItems="center"
+              {activeStep === steps.length ? (
+                <React.Fragment>
+                  <Typography variant="h5" gutterBottom>
+                    Kiitos huollon varauksesta!
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    Huollon tilausnumerosi on 01234567 ja huoltamon x numero on
+                    xxx
+                  </Typography>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  {getStepContent(activeStep)}
+                  <div className={classes.buttons}>
+                    {activeStep !== 0 && (
+                      <Button
+                        onClick={this.handleBack}
+                        className={classes.button}
+                      >
+                        Takasin
+                      </Button>
+                    )}
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.handleNext}
+                      className={classes.button}
                     >
-                        <Typography variant="h4" color="inherit" className={classes.barText}>
-                            Varaa huolto
-                        </Typography>
-                    </Grid>
-
-                </Toolbar>
-
-                <main className={classes.layout}>
-                    <Paper className={classes.paper}>
-                        <Typography component="h5" variant="h5" align="center">
-                            Huollon varaus
-                        </Typography>
-                        <Stepper activeStep={activeStep} className={classes.stepper}>
-                            {steps.map(label => (
-                                <Step key={label}>
-                                    <StepLabel>{label}</StepLabel>
-                                </Step>
-                            ))}
-                        </Stepper>
-                        <React.Fragment>
-                            {activeStep === steps.length ? (
-                                <React.Fragment>
-                                    <Typography variant="h5" gutterBottom>
-                                        Kiitos huollon varauksesta!
-                                    </Typography>
-                                    <Typography variant="subtitle1">
-                                        Huollon tilausnumerosi on 01234567 ja huoltamon x numero on xxx
-                                    </Typography>
-                                </React.Fragment>
-                            ) : (
-                                <React.Fragment>
-                                    {getStepContent(activeStep)}
-                                    <div className={classes.buttons}>
-                                        {activeStep !== 0 && (
-                                            <Button onClick={this.handleBack} className={classes.button}>
-                                                Takasin
-                                            </Button>
-                                        )}
-                                        <Button
-                                            variant="contained"
-                                            color="primary"
-                                            onClick={this.handleNext}
-                                            className={classes.button}
-                                        >
-                                            {activeStep === steps.length - 1 ? 'Varaa huolto' : 'Seuraava'}
-                                        </Button>
-                                    </div>
-                                </React.Fragment>
-                            )}
-                        </React.Fragment>
-                    </Paper>
-                </main>
+                      {activeStep === steps.length - 1
+                        ? "Varaa huolto"
+                        : "Seuraava"}
+                    </Button>
+                  </div>
+                </React.Fragment>
+              )}
             </React.Fragment>
-        );
-    }
+          </Paper>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 Checkout.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Checkout);
